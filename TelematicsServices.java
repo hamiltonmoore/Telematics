8bc4457570eb07d26da6522f6e4d913f23bc6538
hamilton moore <hamlmoor@gmail.com>
2017-09-16 15:07:35 -0500

    added filewriter

--- TelematicsServices.java
+++ TelematicsServices.java
@@ -1,10 +1,24 @@
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-
-import javax.sound.midi.Soundbank;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TelematicsServices {
 
     void report(VehicleInfo vehicleInfo) {
 
+//        public static void main(String[] args) {
+            try {
+                File file = new File(Integer.toString(vehicleInfo.getVin()));   //also tried (vehicleInfo.vin)
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write();
+                fileWriter.write();
+                fileWriter.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
-}
+
